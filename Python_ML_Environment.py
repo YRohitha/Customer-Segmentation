1f51c2608fd4e48844a9669d93fda651ca637c5d
Rohitha Yarlagadda <yrohitha996@gmail.com>
2021-12-27 15:39:35 +0530

    Create Python_ML_Environment.py
    Common Python Libraries and Modules

---
+++ Python_ML_Environment.py
@@ -0,0 +1,29 @@
+
+
+# Python Libraries and Modules
+# 1. Numpy for numerical computation
+import numpy as np
+# 2. Pandas supports data frames
+import pandas as pd
+# 3. Scikit-learn handles numerical matrices
+import scikit-learn as sklearn
+
+# 4. Sampling Helper; train_test_split() function for model selection modules - help us choose between models
+from sklearn.model_selection import train_test_split
+
+# 5. Preprocessing modules - scaling, transforming, wrangling data
+from sklearn import preprocessing
+
+# 6. Import Random Forest; Family of Models : Random Forest, Linear Regression, SVM
+from sklearn.ensemble import RandomForestRegressor
+
+# 7. Import Cross Validation pipeline
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV
+
+# 8. Import Evaluation Metrics
+from sklearn.metrics import mean_squared_error, r2_score
+
+# 9. Import joblib for saving models, similar to pickle
+from sklearn.externals import joblib
+
